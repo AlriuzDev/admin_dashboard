5deb4a7ea912c1d0fb9cdf9cee914150f3be6afa
Anderson <avictoral@gmail.com>
2023-11-04 12:45:09 -0400

    created user Schema

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { getUser } from "../controllers/general.js";
 
 const router = express.Router();
+
+// router.get("/user/:id",getUser);
+router.route("user/:id").get(getUser);
 
 export default router;
