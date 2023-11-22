3b634f3d8c67818d08375dfd8b65538ac11cc1c1
Anderson <avictoral@gmail.com>
2023-11-22 08:02:13 -0500

    created admin component

---
+++ client/src/scenes/admin/index.jsx
@@ -0,0 +1,13 @@
+import { Box, useTheme } from "@mui/material";
+import { useGetAdminsQuery } from "../../state/api";
+import { DataGrid } from "@mui/x-data-grid";
+import Header from "../../components/Header";
+
+const Admin = () => {
+  const theme = useTheme();
+  const { data, isLoading } = useGetAdminsQuery();
+  console.log({ data });
+  return <div>Admin</div>;
+};
+
+export default Admin;
