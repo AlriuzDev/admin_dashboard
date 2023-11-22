83a9978ac38a94853e3a74b0be368d92c6b36245
Anderson <avictoral@gmail.com>
2023-11-22 08:29:46 -0500

    updated transaction component

--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -100,10 +100,14 @@ const Transactions = () => {
           onPageChange={(newPage) => setPage(newPage)}
           onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
           onSortModelChange={(newSortModel) => setSort(...newSortModel)}
-          components={{ Toolbar: DataGridCustomToolbar }}
-          componentsProps={{
+          slots={{ toolbar: DataGridCustomToolbar }}
+          slotProps={{
             toolbar: { searchInput, setSearchInput, setSearch },
           }}
+          // components={{ Toolbar: DataGridCustomToolbar }}
+          // componentsProps={{
+          //   toolbar: { searchInput, setSearchInput, setSearch },
+          // }}
         />
       </Box>
     </Box>
