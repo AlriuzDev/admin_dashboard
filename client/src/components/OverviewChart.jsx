ae47eff24c2aa5c754584139398325a7f7eccec5
Anderson <avictoral@gmail.com>
2023-11-23 08:57:51 -0500

    updated OverviewChart

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -94,6 +94,7 @@ const OverviewChart = ({ isDashboard = false, view }) => {
       }}
       yFormat=" >-.2f"
       curve="catmullRom"
+      enableArea={isDashboard}
       axisTop={null}
       axisRight={null}
       axisBottom={{
@@ -109,6 +110,7 @@ const OverviewChart = ({ isDashboard = false, view }) => {
         legendPosition: "middle",
       }}
       axisLeft={{
+        tickValues: 5,
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
