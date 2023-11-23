b143ab32a4d1e8f0770f5684c02d6fc617663de3
Anderson <avictoral@gmail.com>
2023-11-23 08:03:30 -0500

    updated general routes

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getUser } from "../controllers/general.js";
+import { getUser, getDashboardStats } from "../controllers/general.js";
 
 const router = express.Router();
 
-// router.get("/user/:id",getUser);
 router.route("/user/:id").get(getUser);
+router.route("/dashboard").get(getDashboardStats);
 
 export default router;
